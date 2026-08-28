c46393d33821892271d57784a9e984346acf08c1
Gopikasri-lab <srigopika1969@gmail.com>
2026-08-28 16:52:32 +0530

    Solve ml-gaussian-naive-bayes

--- study-plans/cracking-ml/ml-gaussian-naive-bayes/ml-gaussian-naive-bayes.py
+++ study-plans/cracking-ml/ml-gaussian-naive-bayes/ml-gaussian-naive-bayes.py
@@ -4,34 +4,36 @@ def gaussian_nb(X_train, y_train, X_test):
     """
     Returns: A list of predicted integer labels for each test point
     """
-    X_train = np.asarray(X_train, dtype=float)
-    y_train = np.asarray(y_train, dtype=int)
-    X_test = np.asarray(X_test, dtype=float)
-    n = len(y_train)
+    X_train = np.asarray(X_train,dtype = float)
+    y_train = np.asarray(y_train,dtype = int)
     classes = np.unique(y_train)
-    eps = 1e-9
-
+    n= len(y_train)
     stats = {}
+    e = 1e-9
     for c in classes:
-        mask = y_train == c
-        Xc = X_train[mask]
-        stats[c] = {
-            'prior': np.log(np.sum(mask) / n),
-            'mean': np.mean(Xc, axis=0),
-            'var': np.var(Xc, axis=0) + eps
+        m = y_train==c
+        val = X_train[m]
+        stats[c]={
+            'prior': np.log(len(m)/n),
+            'mean' : np.mean(val, axis=0),
+            'var' : np.var(val,axis=0) + e 
         }
-
+    X_test = np.asarray(X_test,dtype = float)
     predictions = []
     for x in X_test:
+        best_value = -np.inf
         best_class = None
-        best_score = -np.inf
         for c in classes:
             s = stats[c]
-            log_post = s['prior']
-            log_post += np.sum(-0.5 * np.log(2 * np.pi * s['var']) - (x - s['mean'])**2 / (2 * s['var']))
-            if log_post > best_score:
-                best_score = log_post
+
+            logpc = s['prior']
+            logpc+=np.sum((-0.5*np.log(2*np.pi*s['var'])) - (((x-s['mean'])**2)/(2*s['var'])))
+            if logpc > best_value:
+                best_value = logpc
                 best_class = c
         predictions.append(int(best_class))
 
     return predictions
+            
+        
+    pass
